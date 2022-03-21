aee82d69c9bdb39f82d52dc7f2c055039b49b038
Alexander Ryzhov <a1exander@inbox.ru>
2022-03-21 12:49:12 +0300

    Add initial tests for DC VM

---
+++ New-ADDS.Tests.ps1
@@ -0,0 +1,28 @@
+﻿Describe "VM" {
+
+    BeforeAll {
+        $jsonfile = Get-Content ./vars.json -Raw
+        $json = ConvertFrom-Json -InputObject $jsonfile
+        $HyperVCredPath = $json.HyperVCredPath
+        $VMName = $json.VMName
+        $systemDiskPath = $json.systemDiskPath
+        $ADDBDiskPath = $json.ADDBDiskPath
+        #$cred = Import-Clixml -Path $HyperVCredPath
+        #$session = New-PSSession -ComputerName $VMName -Credential $cred
+    }
+
+    AfterAll {
+        #$session | Remove-PSSession
+    }
+
+    Context "Disks" {
+        
+        It "system VHDX in the expected location" {
+            $systemDiskPath | Should -Exist
+        }
+
+        It "ADDB VHX in the expected location" {
+            $ADDBDiskPath | Should -Exist
+        }
+    }
+}
